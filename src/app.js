bfb7489c37558769eb113064a6a020d8a1ae376b
rodrigo <rodrigoscolnic@gmail.com>
2023-03-29 17:24:37 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.set('view engine' , 'ejs');
+app.set('views' , 'src/views');
+
+app.use('/' , require('./routes/mainRouter'));
+
+app.set('puerto',process.env.PORT || 3000)
+
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
---
+++ src/controllers/mainController.js
@@ -0,0 +1,9 @@
+module.exports = {
+    index: (req,res) => {
+        res.render('index')
+    } ,
+
+    menuShow: (req,res) => {
+        res.render('menu')
+    }
+}
